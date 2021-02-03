3d99c48985feac82ef913f537bb7a9e4828988e0
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-02-04 02:40:20 +0300

    init: initialized project

---
+++ Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+        public const int numSamples = 512;
+        public const int numBands = 8;
+
+        public float smoothness;
+
+        [SerializeField] FFTWindow window;
+
+        AudioSource audioSource;
+        float[] samples;
+        float[] bands;
+        float[] buffers;
+        float[] bufferHighest;
+        float[] audioBands;
+
+        void Start()
+        {
+                audioSource = GetComponent<AudioSource>();
+
+                samples = new float[numSamples];
+                bands = new float[numBands];
+                buffers = new float[numBands];
+                bufferHighest = new float[numBands];
+                audioBands = new float[numBands];
+        }
+
+        void Update()
+        {
+                GenerateBands();
+                GenerateDeltas();
+                GenerateAudioBands();
+        }
+
+        void GenerateBands()
+        {
+                audioSource.GetSpectrumData(samples, 0, window);
+                for (int i = 0, count = 0; i < numBands; ++i)
+                {
+                        int sampleCount = (int)Mathf.Pow(2, i + 1);
+
+                        float sum = 0f;
+                        for (int j = 0; j < sampleCount; ++j, ++count)
+                                sum += samples[count] * sampleCount;
+
+                        float average = sum / count;
+                        bands[i] = average;
+                }
+        }
+
+        void GenerateDeltas()
+        {
+                for (int i = 0; i < numBands; ++i)
+                        buffers[i] = Mathf.Lerp(buffers[i], bands[i], Time.deltaTime * smoothness);
+        }
+
+        void GenerateAudioBands()
+        {
+                for (int i = 0; i < numBands; ++i)
+                {
+                        if (buffers[i] > bufferHighest[i])
+                                bufferHighest[i] = buffers[i];
+                        audioBands[i] = buffers[i] / bufferHighest[i];
+                        if (float.IsNaN(audioBands[i]) || float.IsInfinity(audioBands[i]))
+                                audioBands[i] = 0.5f;
+                }
+        }
+
+        public float[] GetSamples() => samples;
+        
+        public float[] GetBands() => bands;
+
+        public float[] GetBuffers() => buffers;
+
+        public float[] GetAudioBands() => audioBands;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+        public int numCubes;
+        public float radius;
+        public float minScale;
+        public float scaleMultiplier;
+
+        [SerializeField] GameObject sampleCubePrefab;
+        GameObject[] sampleCubes;
+        AudioPeer audioPeer;
+
+        void Start()
+        {
+                audioPeer = FindObjectOfType<AudioPeer>();
+                sampleCubes = new GameObject[numCubes];
+
+                float angle = 0f;
+                for (int i = 0; i < numCubes; ++i)
+                {
+                        Vector3 position = new Vector3(Mathf.Cos(-angle * Mathf.Deg2Rad) * radius, transform.position.y, Mathf.Sin(-angle * Mathf.Deg2Rad) * radius);
+                        Quaternion rotation = Quaternion.Euler(0f, angle, 0f);
+
+                        GameObject cube = Instantiate(sampleCubePrefab, position, rotation);
+                        cube.transform.parent = transform;
+                        cube.name = $"Cube #{i}";
+
+                        sampleCubes[i] = cube;
+                        angle += 360f / numCubes;
+                }
+        }
+
+        void Update()
+        {
+                float[] buffers = audioPeer.GetAudioBands();
+                for (int i = 0; i < numCubes; ++i)
+                {
+                        Vector3 scale = new Vector3(transform.localScale.x, minScale + scaleMultiplier * buffers[i], transform.localScale.z);
+                        sampleCubes[i].transform.localScale = scale;
+                }
+        }
+}
